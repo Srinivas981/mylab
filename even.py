28ac08c5eea17e7c4ac1f158c9f07e3f2a68b9b4
Chunduri Sai Srinivas <160648392+Srinivas981@users.noreply.github.com>
2025-01-09 10:19:31 +0530

    even.py

--- even.py
+++ even.py
@@ -1,5 +1,10 @@
-n=5
-if(n%2==0):
+import sys
+
+# Get the number from command-line argument
+n = int(sys.argv[1])
+
+# Check if the number is even or odd
+if n % 2 == 0:
     print("Even number")
 else:
-    print("odd number")
+    print("Odd number")
